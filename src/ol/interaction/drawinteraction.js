a22d42a1d2f525eb0327d474a0ae63061ef43fdf
Tim Schaub <tim.schaub@gmail.com>
2013-10-30 16:07:39 -0600

    Pop off last point from linestring

--- src/ol/interaction/drawinteraction.js
+++ src/ol/interaction/drawinteraction.js
@@ -264,6 +264,13 @@ ol.interaction.Draw.prototype.finishDrawing_ = function(event) {
   this.sketchLayer_.removeFeatures(features);
   this.removeSketchLayer_(event.map);
   sketchFeature.renderIntent = ol.layer.VectorLayerRenderIntent.DEFAULT;
+  var geometry = sketchFeature.getGeometry();
+  if (this.mode_ === ol.interaction.DrawMode.LINESTRING) {
+    var geometry = sketchFeature.getGeometry();
+    var coordinates = geometry.getCoordinates();
+    coordinates.pop();
+    geometry.setCoordinates(coordinates);
+  }
   this.layer_.addFeatures([sketchFeature]);
 };
 
